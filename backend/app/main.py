a686670e5d69050262ddb8c40831ffe7ce5ad6f2
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-06-15 16:43:44 -0300

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from .api.user_router import router as user_router
+from .database.init_db import init_db
+from .core.exception_handler import register_exception_handlers
 
-app = FastAPI()
+app = FastAPI(
+    title="Chatbot API",
+    description="API para gerenciamento do chatbot",
+    version="1.0.0",
+    root_path="/api"
+)
 
-@app.get("/api")
-def read_root():
-    return {"message": "Hello Wooooooorld"}
+register_exception_handlers(app)
+
+@app.on_event("startup")
+def startup_event():
+    init_db()
+
+app.include_router(user_router)
