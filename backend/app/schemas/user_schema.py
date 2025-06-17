c6039fd0fa70589825b7c5d30d610673eed09135
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-06-17 06:54:04 -0300

    update User schema

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel, EmailStr, constr
 from datetime import date
 from typing import Optional
 from ..core.constants.perfil import PerfilEnum
+from ..core.constants.gender import GenderEnum
 
 class UserBase(BaseModel):
     name: str
     email: EmailStr
     cpf: constr(min_length=11, max_length=14) 
-    gender: str
+    gender: GenderEnum
     phone: str
     birth_date: date
     perfil: PerfilEnum = PerfilEnum.USER
@@ -23,7 +24,7 @@ class UserRead(UserBase):
 
 class UserUpdate(BaseModel):
     name: Optional[str] = None
-    gender: Optional[str] = None
+    gender: Optional[GenderEnum] = None
     phone: Optional[str] = None
     birth_date: Optional[date] = None
     perfil: Optional[PerfilEnum] = None
