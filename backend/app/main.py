e3ab3815c4066654710af4859a64f26ffe5ab747
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-06-17 08:10:40 -0300

    add ChatAnswer router in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response
 from .routers.user_router import router as user_router
 from .routers.chat_question_router import router as chat_question_router
+from .routers.chat_answer_router import router as chat_answer_router
 from .auth import auth_router
 from .database.init_db import init_db
 from .core.exception_handler import register_exception_handlers
@@ -20,6 +21,7 @@ def startup_event():
 
 app.include_router(user_router)
 app.include_router(chat_question_router)
+app.include_router(chat_answer_router)
 app.include_router(auth_router.router)
 
 @app.get("/", tags=["Home"])
