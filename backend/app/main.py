da7bee799b254349608b4cca25187b1972a2700b
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-06-23 14:49:31 -0300

    create client router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from .routers.user_router import router as user_router
 from .routers.chat_question_router import router as chat_question_router
 from .routers.chat_answer_router import router as chat_answer_router
 from .routers.chat_router import router as chat_router
+from .routers.client_router import router as client_router
 from .auth import auth_router
 from .database.init_db import init_db
 from .core.exception_handler import register_exception_handlers
@@ -24,6 +25,7 @@ app.include_router(user_router)
 app.include_router(chat_question_router)
 app.include_router(chat_answer_router)
 app.include_router(chat_router)
+app.include_router(client_router)
 app.include_router(auth_router.router)
 
 @app.get("/", tags=["Home"])
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,5 @@ from .chat_answer_model import ChatAnswer
 from .chat_message_model import ChatMessage
 from .chat_question_model import ChatQuestion
 from .chat_session_model import ChatSession
+from .client_model import Client
+from .external_user_model import ExternalUser
